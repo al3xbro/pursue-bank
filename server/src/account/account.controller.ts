304b12c8be4fb3072b7e7608f8ef1cb2930dcc9a
Nathan Nguyen <nathanvinhnguyen@gmail.com>
2024-09-28 15:53:24 -0700

    Get barebones version of account creation working

--- server/src/account/account.controller.ts
+++ server/src/account/account.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { AccountService } from './account.service';
+import { User } from '@prisma/client';
 
 @Controller('account')
-export class AccountController {}
+export class AccountController {
+    constructor(private accountService: AccountService) {}
+
+    @Post()
+    async createAccount(
+        @Body() userData: Omit<User, "id" | "status">
+    ): Promise<User> {
+        return this.accountService.createAccount(
+            {...userData, status: 'ACTIVE'}
+        );
+    }
+}
--- server/src/account/account.service.ts
+++ server/src/account/account.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { Prisma, User } from '@prisma/client';
 
 @Injectable()
-export class AccountService {}
+export class AccountService {
+    constructor(private prisma: PrismaService) {}
+
+    async createAccount(data: Omit<User, "id">): Promise<User> {
+        return this.prisma.user.create({data});
+    }
+}
