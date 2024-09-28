c2e5f3ca0357954afc2d7bfb9bf8c8f460de0310
Nathan Nguyen <nathanvinhnguyen@gmail.com>
2024-09-28 11:03:27 -0700

    Remove unnecessary files and rename project name to 'server'

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AccountModule } from './account/account.module';
 import { TransactionModule } from './transaction/transaction.module';
 import { AutopayModule } from './autopay/autopay.module';
-import { AtmModule } from './atm/atm.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AccountModule, TransactionModule, AutopayModule, AtmModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AccountModule, TransactionModule, AutopayModule, AuthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
