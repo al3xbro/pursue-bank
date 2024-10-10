847c37fd945db80d88fa6b0a38b14738325dedb7
Alex Bui <alexnbui@gmail.com>
2024-10-09 17:48:41 -0700

    redesign home

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
+import _ from 'lodash';
+import LargeButton from '../atoms/LargeButton';
 
 export default function Home() {
 
   const [balance, setBalance] = useState<number | undefined>(undefined);
+  const [transactions, setTransactions] = useState<any[]>([]);
 
   const fetchBalance = async () => {
-    const res = await fetch('http://localhost:3000/api/internal/user/test', {
+    const res = await fetch('http://localhost:3000/api/internal/user/test', { // TODO: change to correct endpoint
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -15,12 +18,21 @@ export default function Home() {
     const json = await res.json();
     setBalance(json.balance);
   }
-  fetchBalance();
 
-  //Frameworks for buttons
-  function handleTransactions() {
-
+  const fetchTransactions = async () => {
+    const res = await fetch('http://localhost:3000/api/internal/user/test', { // TODO: change to correct endpoint
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+      },
+    });
+    const json = await res.json();
+    setTransactions(_.take(json.balance, 10));
   }
+
+  fetchBalance();
+  fetchTransactions();
 
   function handlePayment() {
 
@@ -36,51 +48,29 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center h-[calc(100vh-88px)] justify-center bg-gray-100 gap-5">
-
-
-      <div className="flex flex-col gap-10 w-1/2 h-[calc(100vh-88px)] p-8 bg-indigo-600 rounded-lg shadow-lg">
-        <div className="flex w-full h-full">
-
-           {/* Left Column (3/5 width) */}
-          <div className="flex-3 bg-white text-black p-8 rounded-lg shadow-lg mr-6">
-            {/* Link to backend to display correct balance*/}
-            <h1 className="text-6xl font-bold">{balance?.toLocaleString() ?? 'unable to fetch'}</h1>
-            <h1 className="text-2xl">Current Balance</h1>
-
-            <button
-              onClick={handleTransactions}
-              className="text-3xl font-semibold w-50 mt-4 bg-gray-50 text-black p-20 rounded-md hover:bg-gray-100 transition"
-            >
-              My Transactions
-            </button>
+      <div className="flex gap-20 w-3/5 min-w-96 h-[calc(100vh-88px)] my-32">
+        <div className='flex w-[60%] max-h-fit flex-col gap-8 bg-indigo-600 p-8 rounded-lg'>
+          <div className='flex flex-col justify-between h-[200px] bg-gray-100 w-full rounded-lg p-8 shadow-md'>
+            <div className='font-semibold text-xl'>Balance</div>
+            <div className='font-semibold text-[40pt]'>{balance ?? 'unable to fetch'}</div>
           </div>
-          
-          {/* Right Column (2/5 width) */}
-          <div className="flex-2 bg-white text-black p-8 rounded-lg shadow-lg">
-            <button
-              onClick={handlePayment}
-              className="text-3xl font-semibold w-50 mt-4 bg-gray-50 text-black p-20 rounded-md hover:bg-gray-100 transition"
-            >
-              Make a payment
-            </button>
-
-            <button
-              onClick={handleWithDepo}
-              className="text-3xl font-semibold w-50 mt-4 bg-gray-50 text-black p-20 rounded-md hover:bg-gray-100 transition"
-            >
-              Withdraw/Deposit
-            </button>
-
-            <button
-              onClick={handleTransfer}
-              className="text-3xl font-semibold w-50 mt-4 bg-gray-50 text-black p-20 rounded-md hover:bg-gray-100 transition"
-            >
-              Transfer
-            </button>
+          <div className='flex flex-col justify-between h-[200px] bg-gray-100 w-full rounded-lg p-8 shadow-md'>
+            <div className='font-semibold text-xl'>Transactions</div>
+            <div className='font-semibold text-[40pt]'>{transactions.map((transaction) => transaction.amount) ?? 'unable to fetch'}</div>
           </div>
-
-          
-
+        </div>
+        <div className='flex flex-col w-[40%] gap-8'>
+          <div className='font-semibold text-xl'>Transfer</div>
+          <LargeButton>
+            <div className='text-xl'>
+              Internal
+            </div>
+          </LargeButton>
+          <LargeButton>
+            <div className='text-xl'>
+              External
+            </div>
+          </LargeButton>
         </div>
       </div>
     </div>
