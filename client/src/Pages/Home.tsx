6d49a9610e7b87b64aea0bcfcdd96e200b98cce4
Alex Bui <alexnbui@gmail.com>
2024-10-11 21:34:25 -0700

    refactor frontend fetches

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -1,69 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import _ from 'lodash';
 import LargeButton from '../atoms/LargeButton';
-import { createTransaction } from '../services/transaction';
+import { createTransaction, getBalance, getTransactions } from '../services/transaction';
+import TransactionBar from '../molecules/TransactionBar';
 
 export default function Home() {
 
   const [balance, setBalance] = useState<number | undefined>(undefined);
   const [transactions, setTransactions] = useState<any[]>([]);
 
-  const fetchBalance = async () => {
-    const res = await fetch(`http://localhost:3000/api/internal/transaction/${localStorage.getItem('accountId')}/balance`, { // TODO: change to correct endpoint
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-      },
-    });
-    const json = await res.json();
-    console.log(json);
-    setBalance(json.balance);
-  }
+  useEffect(() => {
+    getBalance().then((res) => setBalance(res.balance))
+    getTransactions().then((res) => setTransactions(res))
+  }, [])
 
-  const fetchTransactions = async () => {
-    const res = await fetch('http://localhost:3000/api/internal/user/test', { // TODO: change to correct endpoint
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-      },
-    });
-    const json = await res.json();
-    setTransactions(_.take(json.balance, 10));
-  }
-
-  fetchBalance();
-  fetchTransactions();
-
-  function handlePayment() {
-
-  }
-
-  function handleWithDepo() {
-
-  }
-
-  function handleTransfer() {
-
-  }
+  console.log(transactions)
 
   return (
-    <div className="flex flex-col items-center h-[calc(100vh-88px)] justify-center bg-gray-100 gap-5">
+    <div className="flex flex-col items-center justify-center bg-gray-100 ">
       <div className="flex gap-20 w-3/5 min-w-96 h-[calc(100vh-88px)] my-32">
         <div className='flex w-[60%] max-h-fit flex-col gap-8 bg-indigo-600 p-8 rounded-lg'>
           <div className='flex flex-col justify-between h-[200px] bg-gray-100 w-full rounded-lg p-8 shadow-md'>
             <div className='font-semibold text-xl'>Balance</div>
             <div className='font-semibold text-[40pt]'>{balance ?? 'unable to fetch'}</div>
           </div>
-          <div className='flex flex-col justify-between h-[200px] bg-gray-100 w-full rounded-lg p-8 shadow-md'>
+          <div className='flex flex-col justify-between bg-gray-100 w-full rounded-lg p-8 shadow-md'>
             <div className='font-semibold text-xl'>Transactions</div>
-            <div className='font-semibold text-[40pt]'>{transactions.map((transaction) => transaction.amount) ?? 'unable to fetch'}</div>
+            <div className='font-semibold text-[40pt]'>{transactions.map((transaction) => <TransactionBar amount={transaction.amount} transactionType={transaction.transactionType} />) ?? 'unable to fetch'}</div>
           </div>
         </div>
         <div className='flex flex-col w-[40%] gap-8'>
           <div className='font-semibold text-xl'>Transfer</div>
-          <LargeButton onClick={() => createTransaction(parseInt(localStorage.getItem('accountId') ?? '0'), 2)}>
+          <LargeButton onClick={() => createTransaction(2)}>
             <div className='text-xl'>
               Internal
             </div>
--- client/src/services/transaction.ts
+++ client/src/services/transaction.ts
@@ -1,21 +1,40 @@
-async function createTransaction(fromId: number, toId: number) {
-  return await fetch('http://localhost:3000/api/internal/transaction', {
+async function createTransaction(toId: number) {
+  const res = await fetch('http://localhost:3000/api/internal/transaction', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
     },
     body: JSON.stringify({
-      transaction_type: 'TRANSFER_INTERNAL',
-      transfer_id: toId,
+      transactionType: 'TRANSFER_INTERNAL',
+      transferId: toId,
       amount: 100,
-      user: {
-        connect: {
-          id: fromId,
-        }
-      },
     }),
   });
+  const json = await res.json();
+  return json;
 }
 
-export { createTransaction };
+async function getBalance() {
+  const res = await fetch('http://localhost:3000/api/internal/transaction/balance', {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+    },
+  });
+  const json = await res.json();
+  return json
+}
+
+async function getTransactions() {
+  const res = await fetch('http://localhost:3000/api/internal/transaction', {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+    },
+  });
+  const json = await res.json();
+  return json
+}
+
+export { createTransaction, getBalance, getTransactions };
