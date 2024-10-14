4772c103896ecda933e9dd300e570737d6fe043b
blching <enternalrice@gmail.com>
2024-10-14 15:20:06 -0700

    Fixed topbar nav

--- client/src/Components/TopBar.tsx
+++ client/src/Components/TopBar.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import AccountIcon from './AccountIcon';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
 
 export default function TopBar() {
   const location = useLocation(); // Get the current location
+  const navigate = useNavigate(); // Initialize navigate
 
   const handleAccountClick = () => {
     console.log('Account icon clicked'); // Need to implement when icon is clicked
@@ -18,10 +20,10 @@ export default function TopBar() {
       <nav className="ml-auto">
         <ul className="flex gap-4">
           <li className='m-auto'>
-            <a href="/" className="hover:text-indigo-300">Home</a>
+            <a href="/" className="hover:text-indigo-300" onClick={() => navigate('/')}>Home</a>
           </li>
           <li className='m-auto'>
-            <a href="/about" className="hover:text-indigo-300">About</a>
+            <a href="/about" className="hover:text-indigo-300" onClick={() => navigate('/about')}>About</a>
           </li>
           {!isLoginPage && <AccountIcon onClick={handleAccountClick} />}
         </ul>
