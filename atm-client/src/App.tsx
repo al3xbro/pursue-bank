a7c5d4b128c4f786d8e5e6080ba1bea4885854f5
Alex Bui <alexnbui@gmail.com>
2024-11-06 17:50:09 -0800

    add invalidate queries + error

--- atm-client/src/App.tsx
+++ atm-client/src/App.tsx
@@ -1,16 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import TransactionPage from './pages/TransactionPage';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
 
 export default function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<TransactionPage />} />
-          <Route path='/login' element={<LoginPage />} />
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<TransactionPage />} />
+            <Route path='/login' element={<LoginPage />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </>
   )
 } 
---
+++ atm-client/src/molecules/TransactionField.tsx
@@ -0,0 +1,48 @@
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import LargeButton from '../atoms/LargeButton';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { deposit, withdraw } from '../services/transactions';
+
+export default function TransactionField() {
+
+  const { register, handleSubmit, formState: { errors } } = useForm()
+
+  const queryClient = useQueryClient()
+  const { mutate: makeDeposit } = useMutation({
+    mutationKey: ['balance'],
+    mutationFn: async (data: FieldValues) => {
+      return deposit(data.amount)
+    },
+    onSuccess: () => queryClient.invalidateQueries()
+  })
+  const { mutate: makeWithdrawal } = useMutation({
+    mutationKey: ['balance'],
+    mutationFn: async (data: FieldValues) => {
+      return withdraw(data.amount)
+    },
+    onSuccess: () => queryClient.invalidateQueries()
+  })
+
+  return (
+    <>
+      <form className='flex justify-between'>
+        <input
+          type='number'
+          className='p-2 border border-gray-300 rounded-md'
+          placeholder='Enter amount'
+          {...register('amount', {
+            required: {
+              value: true,
+              message: 'Please enter an amount.'
+            }
+          })}
+        />
+        <div className='flex gap-5'>
+          <LargeButton text='Deposit' onClick={handleSubmit(makeDeposit as SubmitHandler<FieldValues>)} />
+          <LargeButton text='Withdraw' onClick={handleSubmit(makeWithdrawal as SubmitHandler<FieldValues>)} />
+        </div>
+      </form>
+      <div className='text-red-600'>{errors.amount?.message?.toString()}</div>
+    </>
+  )
+}
