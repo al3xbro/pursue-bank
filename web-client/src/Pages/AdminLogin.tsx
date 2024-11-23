c80360abcdd63b41706b6470534483463f749589
blching <enternalrice@gmail.com>
2024-11-22 18:44:19 -0800

    Fixed bugs adminlogin

--- web-client/src/Pages/AdminLogin.tsx
+++ web-client/src/Pages/AdminLogin.tsx
@@ -32,7 +32,7 @@ export default function Login() {
         localStorage.setItem('accountId', accountId)
 
         // Navigate to home page after successful login
-        navigate('/');
+        navigate('/AdminHome');
       } else {
         // Handle login error (e.g., incorrect password)
         setError(data.message || 'Login failed, please try again');
