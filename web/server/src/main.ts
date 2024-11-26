6fb93cb59556097a15909b1f7afe800b0b25b31c
Alex Bui <alex@alexserver.sytes.net>
2024-11-25 21:11:07 -0800

    prod

--- web/server/src/main.ts
+++ web/server/src/main.ts
@@ -1,13 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/alexserver.sytes.net/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/alexserver.sytes.net/fullchain.pem'),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: httpsOptions
+  });
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  await app.listen(3000);
+  await app.listen(8001, '0.0.0.0');
 }
 bootstrap();
